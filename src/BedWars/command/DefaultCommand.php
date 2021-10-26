42c80ef330cc9adcf01a404e8093e14ced7e0d89
Nobody <83214438+DanteDEVS@users.noreply.github.com>
2021-10-26 18:34:34 +0200

    more errors..

--- src/BedWars/command/DefaultCommand.php
+++ src/BedWars/command/DefaultCommand.php
@@ -59,7 +59,6 @@ class DefaultCommand extends PluginCommand
 			switch (strtolower($args[0])) {
 				case "list";
 					$sender->sendMessage(TextFormat::BOLD . TextFormat::DARK_RED . "Arena List");
-					foreach ($this->getPlugin()->game as $game) {
 						$sender->sendMessage(TextFormat::GRAY . "- " . TextFormat::GREEN . $game->getName() . " [" . count($game->players) . "/" . $game->getMaxPlayers() . "]");
 					}
 					break;
@@ -75,7 +74,6 @@ class DefaultCommand extends PluginCommand
 					}
 
 					$gameName = $args[1];
-					if (array_key_exists($gameName, $this->getPlugin()->game)) {
 						$sender->sendMessage(BedWars::PREFIX . TextFormat::YELLOW . "Game called " . $gameName . " already exists!");
 						return;
 					}
@@ -146,13 +144,11 @@ class DefaultCommand extends PluginCommand
 					}
 
 					$gameName = $args[1];
-					if (!in_array($gameName, array_keys($this->getPlugin()->game))) {
 						$sender->sendMessage(BedWars::PREFIX . TextFormat::YELLOW . "Game called " . $gameName . " doesn't exist!");
 						return;
 					}
 
 					//close the arena if it's running
-					$gameObject = $this->getPlugin()->game[$gameName];
 					if (!$gameObject instanceof Game) {
 						return; //wtf ??
 					}
