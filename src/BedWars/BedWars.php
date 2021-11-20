57f7192652265488705567b0fd4e4356f334c0aa
DEVS <83214438+DanteDEVS@users.noreply.github.com>
2021-11-21 01:14:44 +0200

    Added Final Kills as a new Feature soon

--- src/BedWars/BedWars.php
+++ src/BedWars/BedWars.php
@@ -53,6 +53,7 @@ class BedWars extends PluginBase
     public $staticRestartTime;
     public $eliminations = [];
     public $eliminationsb = [];
+    public $eliminationsf = [];
 
     public static function getConfigs(string $value)
     {
@@ -206,9 +207,21 @@ class BedWars extends PluginBase
             return $this->eliminationsb[$player->getName()];
         } else {
             return $this->eliminationsb[$player->getName()] = 0;
+    }
+    public function addEliminationsf(Player $player): void
+    {
+        if (isset($this->eliminationsf[$player->getName()])) {
+            $this->eliminationsf[$player->getName()] = $this->eliminationsb[$player->getName()] + 1;
+    {
+    public function getEliminationsf(Player $player): int
+    {
+        if (isset($this->eliminationsf[$player->getName()])) {
+            return $this->eliminationsf[$player->getName()];
+        } else {
+            return $this->eliminationsf[$player->getName()] = 0;
+    }           
         }
     }
-
     public function writeArenaData(string $gameName, array $gameData): void
     {
         $location = $this->getDataFolder() . "arenas/" . $gameName . ".json";
